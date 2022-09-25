7ff8a583048794e05fef9edf955ebd517b313422
sowon <thdnjs3596@gmail.com>
2022-09-26 02:32:18 +0900

    * css 수정

--- src/server.js
+++ src/server.js
@@ -7,12 +7,10 @@ dotenv.config();
 const __dirname = path.resolve();
 
 const API_KEY = process.env.WEATHER_API_KEY;
-console.log(API_KEY);
 const app = express();
 
-console.log("hello");
-
-app.set("view engine", "pug");
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 app.set("views", __dirname + "/src/views");
 app.use("/public", express.static(__dirname+"/src/public"));
 app.get("/", (req, res) => res.render("index.html"));
@@ -24,11 +22,9 @@ app.listen(3000, handleListen);
 function getWeatherData(res, lat, lon) {
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
 
-
   return fetch(url)		 // 올바른 url요청을 보낸다
     .then(res => res.json()) 
     .then((result) => {
-        console.log(result)
         res.json({
           city: result.name,
           temp: result.main.temp,
